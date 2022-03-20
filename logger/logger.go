3736c6f6f14c7828a76b0172b5470a57a7e3e64a
vandy ahmad <vandyahmad2404@gmail.com>
2022-03-20 11:18:46 +0700

    update logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,16 +1,44 @@
 package logger
 
 import (
+	"log"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
-func InitLogrus() *logrus.Logger {
-	logger := logrus.New()
-	logger.SetFormatter(&logrus.JSONFormatter{})
+type Logger struct {
+	Logger *logrus.Logger
+}
+
+func NewLogger() *Logger {
+	loggrus := logrus.New()
+	loggrus.SetFormatter(&logrus.JSONFormatter{})
 	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	logger.SetOutput(file)
-	logger.SetFormatter(&logrus.JSONFormatter{})
-	return logger
+	loggrus.SetOutput(file)
+	loggrus.SetFormatter(&logrus.JSONFormatter{})
+
+	return &Logger{
+		Logger: loggrus,
+	}
+}
+
+func (l *Logger) LogFatal(args interface{}) {
+	l.Logger.Fatal(args)
+	log.Fatal(args)
+}
+
+func (l *Logger) LogDebug(args interface{}) {
+	l.Logger.Debug(args)
+	log.Println(args)
+}
+
+func (l *Logger) LogInfo(args interface{}) {
+	l.Logger.Info(args)
+	log.Println(args)
+}
+
+func (l *Logger) LogWarn(args interface{}) {
+	l.Logger.Warn(args)
+	log.Println(args)
 }
--- main.go
+++ main.go
@@ -4,13 +4,22 @@ import (
 	"fmt"
 	cfg "startup/config"
 	"startup/logger"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
 	fmt.Println("halo")
 	config := cfg.GetConfig()
-	logger := logger.InitLogrus()
-	logger.Info("INI info")
+	logger := logger.NewLogger()
 	fmt.Println(config.Database.Startup.Mysql)
+
+	dsn := "root:@tcp(127.0.0.1:3306)/startup?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logger.LogFatal(err.Error())
+	}
+	logger.LogInfo("success connect to database")
 
 }
